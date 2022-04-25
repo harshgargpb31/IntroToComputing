2f818fe6e55a827c8e6c5dd0933d8b59c43e373d
harshgargpb31 <103277112+harshgargpb31@users.noreply.github.com>
2022-04-25 15:29:49 +0530

    Add files via upload

---
+++ output1.py
@@ -0,0 +1,46 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: C:\Users\Dell\OneDrive\Desktop\assignment1.py ============
+solving Q1  
+
+enter no 1 69
+enter no 2 169
+enter no 3 269
+sum of given nos is  507
+average of given nos is  169.0 
+
+solving Q2  
+
+ All values are to be entered in dollars.
+enter your gross income $100000
+enter no of dependents 3
+the tax comes out to be $ 16200.0 
+
+solving Q3  
+
+Student = [sid, name, gender, course, cgpa]
+enter your SID 21102042
+enter your name Harsh
+enter your gender 'M', 'F', 'O' for male, female, others respectively.M
+enter your course name Civil
+enter your cgpa 7.5
+student info  [21102042, 'Harsh', 'M', 'Civil', 7.5] 
+
+solving Q4 
+
+enter marks of student 1 25
+enter marks of student 2 65
+enter marks of student 3 78
+enter marks of student 4 46
+enter marks of student 5 39
+marks of 5 students are  [25, 65, 78, 46, 39]
+marks in ascending order  [25, 39, 46, 65, 78]
+marks in descending order  [78, 65, 46, 39, 25] 
+
+solving Q5 
+
+The provided list is  ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow'] 
+
+ The modified list  is color 1 = ['Red', 'Green', 'White', 'Pink', 'Yellow']
+The modified list is color 2 = ['Red', 'Green', 'White', 'Purple', 'Yellow']
