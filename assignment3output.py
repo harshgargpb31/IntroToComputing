e9ba13b42e351e6fb32cb5a5bbc855f26f3c53d6
harshgargpb31 <103277112+harshgargpb31@users.noreply.github.com>
2022-06-09 22:58:32 +0530

    Add files via upload

---
+++ assignment3output.py
@@ -0,0 +1,46 @@
+Python 3.9.13 (tags/v3.9.13:6de2ca5, May 17 2022, 16:36:42) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+
+===== RESTART: C:\Users\Hp\AppData\Local\Programs\Python\Python310\a-3.1.py ====
+>>> 
+Enter a number to convert it into its binary equivalent:98
+ Its Binary value:
+01100010
+>>> 
+===== RESTART: C:\Users\Hp\AppData\Local\Programs\Python\Python310\a3.3.py =====
+Select an operation to perform : 
+1. ADD
+2. SUBTRACT
+3. MULTIPLY
+4. DIVIDE
+3
+Enter first number: 10
+Enter second number: 7
+The product is :70
+>>> 
+===== RESTART: C:\Users\Hp\AppData\Local\Programs\Python\Python310\a3.2.py =====
+Enter the value of a :3
+Enter the value of b :4
+Enter the value of c :5
+Ans = 35
+Enter the value of n :5
+Ans = 10.0
+Ans = 4*math.pi*r**2
+Ans = math.sqrt(r*(math.cos(a))**2+r*(math.sin(a))**2
+Ans = y2-y1)/(x2-x1
+>>> 
+====== RESTART: C:\Users\Hp\AppData\Local\Programs\Python\Python310\a.4.py =====
+0 1 2 3 4 
+3 4 5 6 7 8 9 
+4 7 10 
+15 13 11 9 7 
+7
+>>> 
+====== RESTART: C:\Users\Hp\AppData\Local\Programs\Python\Python310\a.5.py =====
+This program finds the total molecular weight of a combination of hydrogen, carbon and oxygen atoms
+How many hydrogen atoms? 4
+Carbon? 1
+Oxygen? 1
+The total molecular weight is 32.04186
+>>> 
